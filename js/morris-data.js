00e5287517a76dfec1859e8606cc86a841e354cf
Stephen McCullough <stephen@sixdegreelabs.com>
2015-05-17 16:15:20 +0100

    #1 Added dotfiles commit data

--- js/morris-data.js
+++ js/morris-data.js
@@ -3,53 +3,87 @@ $(function() {
     Morris.Area({
         element: 'morris-area-chart',
         data: [{
-            period: '2015-05-03',
+            period: '2015-02-07',
+            dotfiles: 1585,
             iphone: 2666,
             ipad: null,
             itouch: 2647
         }, {
-            period: '2015-05-04',
+            period: '2015-02-14',
+            dotfiles: 1584,
             iphone: 2778,
             ipad: 2294,
             itouch: 2441
         }, {
-            period: '2015-05-05',
+            period: '2015-02-21',
+            dotfiles: 1587,
             iphone: 4912,
             ipad: 1969,
             itouch: 2501
         }, {
-            period: '2015-05-06',
+            period: '2015-02-28',
+            dotfiles: 1590,
             iphone: 3767,
             ipad: 3597,
             itouch: 5689
         }, {
-            period: '2015-05-07',
+            period: '2015-03-07',
+            dotfiles: 1569,
             iphone: 6810,
             ipad: 1914,
             itouch: 2293
         }, {
-            period: '2015-05-08',
+            period: '2015-03-14',
+            dotfiles: 1506,
             iphone: 5670,
             ipad: 4293,
             itouch: 1881
         }, {
-            period: '2015-05-09',
+            period: '2015-03-21',
+            dotfiles: 1524,
             iphone: 4820,
             ipad: 3795,
             itouch: 1588
         }, {
-            period: '2015-05-10',
+            period: '2015-03-28',
+            dotfiles: 1524,
             iphone: 15073,
             ipad: 5967,
             itouch: 5175
         }, {
-            period: '2015-05-11',
+            period: '2015-04-04',
+            dotfiles: 1532,
             iphone: 10687,
             ipad: 4460,
             itouch: 2028
         }, {
-            period: '2015-05-12',
-            dotfiles: '2',
+            period: '2015-04-11',
+            dotfiles: 1543,
+            ipad: 5713,
+            itouch: 1791
+        }, {
+            period: '2015-04-18',
+            dotfiles: 956,
+            ipad: 5713,
+            itouch: 1791
+        }, {
+            period: '2015-04-25',
+            dotfiles: 1005,
+            ipad: 5713,
+            itouch: 1791
+        }, {
+            period: '2015-05-02',
+            dotfiles: 1005,
+            ipad: 5713,
+            itouch: 1791
+        }, {
+            period: '2015-05-09',
+            dotfiles: 1005,
+            ipad: 5713,
+            itouch: 1791
+        }, {
+            period: '2015-05-16',
+            dotfiles: 1012,
             ipad: 5713,
             itouch: 1791
         }],
